463995f73b3012faa92e1c07256c3e2ec0d2e4bb
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-11-08 19:09:02 +0100

    added bubble sort function !

--- assignment.py
+++ assignment.py
@@ -7,3 +7,17 @@ Created on Thu Nov  8 19:06:23 2018
 """
 
 #hello this s first commit 
+
+#%%
+
+# Bubble sort implementation.
+#
+# This function returns nothing because it works by mutating the lst.
+def bubble(lst):
+    for j in range(0, len(lst) - 1):
+        for i in range(0, len(lst) - j - 1):
+            if lst[i] > lst[i + 1]:
+                temp = lst[i]
+                lst[i] = lst[i + 1]
+                lst[i + 1] = temp
+                
